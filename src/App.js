e61f7550c0e5c627c7c5b68db2d487e8296db5aa
TomE Windows PC <TEbert@seznam.cz>
2019-11-26 22:09:12 +0100

    Clean up of App.js, removed functions replaced by adding redux reducers and actions

--- src/App.js
+++ src/App.js
@@ -16,11 +16,6 @@ import WebDataPage from "./components/webDataPage";
 import UsersPage from "./components/usersPage";
 import { fetch_data } from "./store/actions/fetchServerDataActions";
 import { connect } from "react-redux";
-import {
-  deepClone,
-  validate,
-  saveAndDisplayInputErrors
-} from "./utils/functions";
 import "react-toastify/dist/ReactToastify.css";
 
 class App extends Component {
@@ -34,94 +29,14 @@ class App extends Component {
     this.props.loadDataFromServer();
   }
 
-  handleChangeForCheckbox = e => {
-    const name = e.currentTarget.name;
-    const clickedValue = e.currentTarget.value;
-
-    let newValue = clickedValue === "checked" ? "" : "checked";
-    this.setState({ [name]: newValue });
-  };
-
-  // for any input field
-  handleChangeForInput = e => {
-    const name = e.currentTarget.name;
-    const value = e.currentTarget.value;
-
-    // check for input errors
-    const error = validate(value, name);
-
-    // show and save error or delete it if error has been fixed
-    const errors = saveAndDisplayInputErrors(error, name, {
-      ...this.state.errors
-    });
-
-    this.setState({ errors, [name]: value });
-  };
-
-  // for any input field
-  handleChangeForUserInputFields = (e, facebook_id) => {
-    const name = e.currentTarget.name;
-    const value = e.currentTarget.value;
-    const currentUserID = facebook_id;
-
-    // check for input errors
-    const error = validate(value, name);
-    const errors = saveAndDisplayInputErrors(error, name, {
-      ...this.state.errors
-    });
-
-    // update user data
-    const usersData = [...this.state.usersData];
-    const index = usersData.findIndex(
-      user => user.facebook_id === currentUserID
-    );
-    const cloneUser = usersData[index];
-    cloneUser[name] = value;
-    this.setState({ errors, usersData });
-  };
-
-  handleSaveChanges = async facebook_id => {
-    const usersData = this.props.usersData;
-
-    const index = usersData.findIndex(user => user.facebook_id === facebook_id);
-
-    const dataToServer = JSON.stringify(usersData[index]);
-
-    const errors = this.state.errors;
-    if (Object.keys(errors).length > 0) {
-      toast.error(
-        "Pole " + Object.keys(errors)[0] + " obsahuje nepovolené znaky."
-      );
-      return;
-    }
-
-    try {
-      let data = await http.post(apiEndPoint + "update_user_data.php", {
-        data: dataToServer
-      });
-
-      data = $.parseJSON(data);
-      toast.success("Změny byly uloženy.");
-      //update backup
-      const clone = deepClone(this.state.usersData);
-      this.setState({ usersDataBackup: clone });
-    } catch (error) {
-      handleAjaxError(error);
-      this.setState({ usersData: this.state.usersDataBackup });
-    }
-  };
-
   render() {
     const {
       usersData,
       usersDataBackup,
-      webData,
       usersDataInputErrors,
       inputs,
       inputsErrors
     } = this.props;
-
-    const { rowsToDisplayCheckbox, activityCheckbox } = this.state;
 
     return (
       <React.Fragment>
