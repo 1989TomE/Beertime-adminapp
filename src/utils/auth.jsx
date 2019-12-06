f5c68252f64db5170b2025929f9d48152eebc7d9
TomE Windows PC <TEbert@seznam.cz>
2019-12-06 21:16:12 +0100

    Code shortened in is LoggedIn function

--- src/utils/auth.jsx
+++ src/utils/auth.jsx
@@ -2,11 +2,11 @@ import jwtDecode from "jwt-decode";
 
 export const isLoggedIn = () => {
   try {
-    // token must be set, max 3600 sec for session
+    // token must be set, expires in 7 days
     const token = localStorage.getItem("token");
     const decoded = jwtDecode(token);
     const sevenDaysBefore = new Date().getTime() / 1000 - 3600 * 24 * 7;
-    return token !== null && decoded.time > sevenDaysBefore ? true : false;
+    return token !== null && decoded.time > sevenDaysBefore;
   } catch (error) {
     return false;
   }
